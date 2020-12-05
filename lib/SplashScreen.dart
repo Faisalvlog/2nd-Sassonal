df0567f47480968bb027aee7f85f5ea1cd67309f
Faisalvlog <mfaisal.mfaisal496@gmail.com>
2020-12-05 17:07:43 +0500

    add shimmer liberary for fonts and than make a splash liberary.....full saplash screen commit...

--- lib/SplashScreen.dart
+++ lib/SplashScreen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:ui';
 import 'main.dart';
+import 'package:shimmer/shimmer.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -21,7 +22,7 @@ class _SplashScreenState extends State<SplashScreen> {
 
   onDoneLoading() {
     Navigator.of(context)
-        .pushReplacement(MaterialPageRoute(builder: (context) => HomeScreen()));
+        .pushReplacement(MaterialPageRoute(builder: (context) => MyApp()));
   }
 
   @override
@@ -29,3 +30,43 @@ class _SplashScreenState extends State<SplashScreen> {
     super.dispose();
     loadData();
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: Center(
+        child: Container(
+          alignment: Alignment.center,
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [Colors.lime, Colors.lightBlue],
+            ),
+          ),
+          child: Shimmer.fromColors(
+            baseColor: Color(0xff7f00ff),
+            highlightColor: Color(0xffe100ff),
+            child: Text(
+              'Let\'s play',
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                fontSize: 60,
+                shadows: <Shadow>[
+                  Shadow(
+                    blurRadius: 18,
+                    color: Colors.black87,
+                    offset: Offset.fromDirection(90, 2),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
