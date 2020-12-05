5a3c1add9c650c70afce92bc44501628819bfaf3
Faisalvlog <mfaisal.mfaisal496@gmail.com>
2020-12-05 12:23:33 +0500

    make myapp function

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
+void MyApp(){
+  return runApp(
+      MaterialApp(),
+  );
+}
+
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
