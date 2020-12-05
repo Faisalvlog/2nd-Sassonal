27dba0869c4d81c6d62df01a5e0564434d3ff08e
Faisalvlog <mfaisal.mfaisal496@gmail.com>
2020-12-05 17:18:19 +0500

    add hard file data game full as easy method....

--- lib/hard.dart
+++ lib/hard.dart
@@ -1,11 +1,66 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('HardGame'),
+          backgroundColor: Colors.green,
+        ),
+        body: hard(),
 
-class hard extends StatelessWidget {
+      ),
+    ),
+  );
+}
+
+class hard extends StatefulWidget {
+  @override
+  _hardState createState() => _hardState();
+}
+
+class _hardState extends State<hard> {
+
+  int left_images=1;
+  int right_images=1;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("HardGame"),
+
+
+
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                setState(() {
+                  left_images= Random().nextInt(5)+1;
+                });
+
+                print('Value $left_images');
+              },
+              child: Image.asset("images/dice$left_images.png"),
+            ),
+          ),
+
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                setState(() {
+                  right_images= Random().nextInt(5)+1;
+                });
+                print('SP17-BCS-038');
+              },
+              child: Image.asset(
+                  "images/dice$right_images.png"),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
